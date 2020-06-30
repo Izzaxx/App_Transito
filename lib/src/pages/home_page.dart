e567f04ff47e0124b17451a8cfe620ddd8d4990b
Izzaxx <luisangelizaguirrehernandez@gmail.com>
2020-06-29 21:50:23 -0700

    nada

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -4,7 +4,7 @@ import 'package:trancito_app/src/widgets/drawer.dart';
 import 'package:trancito_app/src/widgets/pdf_view.dart';
 
 class HomePage extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
