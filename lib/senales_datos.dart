1f2170197ec62540cfd4b3352041c11963f19f50
Francisco Flores <franky97_8@hotmail.com>
2020-07-02 17:59:13 -0700

    Se a'adio la tarjeta (no funcional aun)

---
+++ lib/senales_datos.dart
@@ -0,0 +1,18 @@
+class Senales {
+  final String nombre;
+  final String descripcion;
+
+  const Senales({this.nombre, this.descripcion});
+
+}
+
+  const xSenales = const <Senales>[
+    const Senales(
+      nombre: 'Alto', 
+      descripcion: 'Es para hacer alto xd'
+    ),
+    const Senales(
+      nombre: 'Cuidado',
+      descripcion: 'Es para tener cuidado xdd'
+    )
+  ];
---
+++ lib/señales_vista.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class _SenalesItems extends StatelessWidget {
+  final Senales _contact;
+
+  _SenalesItems(this._contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Card(
+            clipBehavior: Clip.antiAlias,
+            child: Column(
+              children: [
+                ListTile(
+                  title: const Text('Card title 1'),
+                  subtitle: Text(
+                    'Secondary Text',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    'Greyhound divisively hello coldly wonderfully marginally far upon excluding.',
+                    style: TextStyle(color: Colors.black.withOpacity(0.6)),
+                  ),
+                ),
+                Image.asset('assets/cantfly.png'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
